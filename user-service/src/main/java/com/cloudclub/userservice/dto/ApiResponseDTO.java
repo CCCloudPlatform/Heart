5bf3d03bf0b3f201b313b65eaef4405487d404b7
김태헌 <57055730+Jake-huen@users.noreply.github.com>
2024-11-06 16:03:17 +0900

    #5 [User-Service] [fix] Swagger 사용 위해 ApiResponse 클래스 이름 수정
    Annotation type expected 오류로 인하여 클래스 이름과 어노테이션 이름 다르게 설정

--- user-service/src/main/java/com/cloudclub/userservice/dto/ApiResponseDTO.java
+++ user-service/src/main/java/com/cloudclub/userservice/dto/ApiResponseDTO.java
@@ -5,21 +5,21 @@ import lombok.Data;
 
 @Data
 @Builder
-public class ApiResponse<T> {
+public class ApiResponseDTO<T> {
     private String status;
     private String message;
     private T data;
 
-    public static <T> ApiResponse<T> success(String message, T data) {
-        return ApiResponse.<T>builder()
+    public static <T> ApiResponseDTO<T> success(String message, T data) {
+        return ApiResponseDTO.<T>builder()
                 .status("success")
                 .message(message)
                 .data(data)
                 .build();
     }
 
-    public static <T> ApiResponse<T> error(String message) {
-        return ApiResponse.<T>builder()
+    public static <T> ApiResponseDTO<T> error(String message) {
+        return ApiResponseDTO.<T>builder()
                 .status("error")
                 .message(message)
                 .build();
